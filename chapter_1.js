7b8278b8d07cfb9900d650f491bd68b3053685f4
Niamh <niamh.linguist@gmail.com>
2023-11-14 09:56:13 +0200

    instructions chpt 1 ex 3

--- chapter_1.js
+++ chapter_1.js
@@ -15,15 +15,15 @@ Write a loop that makes seven calls to console.log to output the following trian
 
 */
 
-let str = "#";
-for (let i = 0; i < 7; i++) {
-  console.log(str);
-  str += "#";
-}
+// let str = "#";
+// for (let i = 0; i < 7; i++) {
+//   console.log(str);
+//   str += "#";
+// }
 
 /*
 
-***********TASK ONE***********
+***********TASK TWO***********
 
 FizzBuzz
 Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
@@ -34,11 +34,31 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 
 */
 
-for (num = 1; num < 100; num++) {
-  let str = "";
+// for (num = 1; num < 100; num++) {
+//   let str = "";
 
-  if (num % 3 === 0) str += "Fizz";
-  if (num % 5 === 0) str += "Buzz";
-  if (!str) str = num;
-  console.log(str);
-}
+//   if (num % 3 === 0) str += "Fizz";
+//   if (num % 5 === 0) str += "Buzz";
+//   if (!str) str = num;
+//   console.log(str);
+// }
+
+/*
+
+***********TASK THREE***********
+
+Chessboard
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+*/
