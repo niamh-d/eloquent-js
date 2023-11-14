525275734756289830ea5f88ca255e87282c710a
Niamh <niamh.linguist@gmail.com>
2023-11-14 09:51:35 +0200

    chpt 1 ex 2 fizzbuzz

--- chapter_1.js
+++ chapter_1.js
@@ -20,3 +20,25 @@ for (let i = 0; i < 7; i++) {
   console.log(str);
   str += "#";
 }
+
+/*
+
+***********TASK ONE***********
+
+FizzBuzz
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+(This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+
+*/
+
+for (num = 1; num < 100; num++) {
+  let str = "";
+
+  if (num % 3 === 0) str += "Fizz";
+  if (num % 5 === 0) str += "Buzz";
+  if (!str) str = num;
+  console.log(str);
+}
